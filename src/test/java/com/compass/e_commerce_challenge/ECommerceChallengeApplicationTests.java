49d0815162f92f09c7e7fa8b6e2e5257a52ccc39
GustavoBeckSilva <gustavogbks@gmail.com>
2025-05-16 17:36:18 -0300

    chore: initial Spring Boot project setup with Web, JPA, Security, Validation, Lombok, PostgreSQL, JJWT, application.properties config and package structure

---
+++ src/test/java/com/compass/e_commerce_challenge/ECommerceChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.e_commerce_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
